e14e64bdf201b985847f784d84cbb5df57169ad4
Hariharan <147143971+hariharans07@users.noreply.github.com>
2024-08-16 14:11:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
